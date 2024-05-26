de1504829b7401bca68dc923a5b8726d595f21f7
Rahat73 <rahat.ashik.18@gmail.com>
2024-05-26 21:24:18 +0600

    routes moduled

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,13 @@ import { StudentRouter } from './app/modules/student/student.route';
 import { UserRouter } from './app/modules/user/user.route';
 import golbalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFoundHandler from './app/middlewares/notFoundHandler';
+import router from './app/routes';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/v1/students', StudentRouter);
-app.use('/api/v1/users', UserRouter);
+app.use('/api/v1', router);
 
 app.get('/test', (req: Request, res: Response) => {
   const test = 'Hello World!';
