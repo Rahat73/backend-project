4bf6c4e7ff0f033c68cff819073e97eda7b338a8
Rahat73 <rahat.ashik.18@gmail.com>
2024-05-27 01:08:54 +0600

    higher order function create async

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,27 +1,25 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from 'express';
 import { UserService } from './user.service';
 import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
 
-const createStudent = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const { password, student } = req.body;
-    const result = await UserService.createStudentIntoDB(password, student);
-
-    sendResponse(res, {
-      statusCode: httpStatus.OK,
-      success: true,
-      message: 'Student created successfully',
-      data: result,
-    });
-  } catch (error) {
-    next(error);
-  }
+const createAsync = (fn: RequestHandler) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+  };
 };
+
+const createStudent = createAsync(async (req, res, next) => {
+  const { password, student } = req.body;
+  const result = await UserService.createStudentIntoDB(password, student);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student created successfully',
+    data: result,
+  });
+});
 
 export const UserController = {
   createStudent,
