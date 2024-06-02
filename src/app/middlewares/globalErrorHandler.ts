95bb8ca30381eefb9728c4b8a17834d302f54553
Rahat73 <rahat.ashik.18@gmail.com>
2024-06-02 19:27:20 +0600

    How To Convert Zod Error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,19 +1,46 @@
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
 import httpStatus from 'http-status';
+import { ZodError, ZodIssue } from 'zod';
+import { TErrorSources } from '../interface/error';
+import config from '../config';
 
-const golbalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
+const golbalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
   let message = err.message || 'Internal Server Error';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  const handleZodError = (err: ZodError) => {
+    const errorSouces: TErrorSources = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue?.path.length - 1],
+        message: issue?.message,
+      };
+    });
+
+    return {
+      statusCode: httpStatus.BAD_REQUEST,
+      message: 'Validation Error',
+      errorSouces,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const formattedError = handleZodError(err);
+    statusCode = formattedError?.statusCode;
+    message = formattedError?.message;
+    errorSources = formattedError?.errorSouces;
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
-    error: err,
+    errorSources,
+    stack: config.NODE_ENV === 'development' ? err.stack : null,
   });
 };
 
