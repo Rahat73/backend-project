5fb128b6e35997fa853e3bc314e17e2c69349c8c
Rahat73 <rahat.ashik.18@gmail.com>
2024-06-12 00:53:55 +0600

    add password changed validation in auth

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -4,44 +4,64 @@ import catchAsync from '../utils/catchAsync';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import { TUserRole } from '../modules/user/user.interface';
+import { User } from '../modules/user/user.model';
 
 export const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req, res, next) => {
     const token = req.headers.authorization;
 
-    // check if the token is sent from the client
+    // checking if the token is missing
     if (!token) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
+    }
+
+    // checking if the given token is valid
+    const decoded = jwt.verify(
+      token,
+      config.jwt_access_secret as string,
+    ) as JwtPayload;
+
+    const { role, userId, iat } = decoded;
+
+    // checking if the user is exist
+    const user = await User.isUserExistsByCustomId(userId);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
+    }
+    // checking if the user is already deleted
+
+    const isDeleted = user?.isDeleted;
+
+    if (isDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
+    }
+
+    // checking if the user is blocked
+    const userStatus = user?.status;
+
+    if (userStatus === 'blocked') {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !');
+    }
+
+    if (
+      user.passwordChangedAt &&
+      User.isJWTIssuedBeforePasswordChanged(
+        user.passwordChangedAt,
+        iat as number,
+      )
+    ) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
+    }
+
+    if (requiredRoles && !requiredRoles.includes(role)) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
-        'Not authorized to access this route',
+        'You are not authorized  hi!',
       );
     }
 
-    // check if the token is valid
-    jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-      function (err, decoded) {
-        if (err) {
-          throw new AppError(
-            httpStatus.UNAUTHORIZED,
-            'Not authorized to access this route',
-          );
-        }
-
-        // check if the token is valid and the role is valid
-        if (
-          requiredRoles.length &&
-          !requiredRoles.includes((decoded as JwtPayload)?.role)
-        ) {
-          throw new AppError(
-            httpStatus.UNAUTHORIZED,
-            'Not authorized to access this route',
-          );
-        }
-        req.user = decoded as JwtPayload;
-        next();
-      },
-    );
+    req.user = decoded as JwtPayload;
+    next();
   });
 };
