d80141d64efc3f470571c30beef626fab05b6ee7
Rahat73 <rahat.ashik.18@gmail.com>
2024-08-16 11:57:04 +0600

    admin route authorization update

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -3,19 +3,33 @@ import { updateAdminValidationSchema } from './admin.validation';
 import validationHandler from '../../middlewares/validationHandler';
 import { AdminControllers } from './admin.controller';
 import { auth } from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constant';
 
 const router = express.Router();
 
-router.get('/', auth(), AdminControllers.getAllAdmins);
+router.get(
+  '/',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.getAllAdmins,
+);
 
-router.get('/:id', AdminControllers.getAdminById);
+router.get(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.getAdminById,
+);
 
 router.patch(
   '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
   validationHandler(updateAdminValidationSchema),
   AdminControllers.updateAdmin,
 );
 
-router.delete('/:id', AdminControllers.deleteAdmin);
+router.delete(
+  '/:id',
+  auth(USER_ROLE.superAdmin, USER_ROLE.admin),
+  AdminControllers.deleteAdmin,
+);
 
 export const AdminRoutes = router;
